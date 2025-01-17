8675f6a0bc9c033661a4b89db561078d6000364f
William-Dev <williamlegall5@gmail.com>
2025-01-17 15:53:51 +0100

    Test

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,21 @@ import "./index.css";
 import Cv from "./componants/Cv/cv.tsx";
 import Acceil from "./componants/Acceil.tsx";
 
+const BASENAME = "/Portfolio"; // Nom du repo sur GitHub Pages
+
 const router = createBrowserRouter([
   {
-    children: [
-      {
-        path: "/",
-        element: <Acceil />,
-      },
-      {
-        path: "/Cv",
-        element: <Cv />,
-      },
-    ],
+    path: `${BASENAME}/`, // Ajoute le basename manuellement
+    element: <Acceil />,
+  },
+  {
+    path: `${BASENAME}/Cv`, // Préfixe aussi ici
+    element: <Cv />,
   },
 ]);
 
 const rootElement = document.getElementById("root");
-if (rootElement == null) {
+if (!rootElement) {
   throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
 
