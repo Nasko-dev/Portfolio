b856ad25bf5297031b97e25377f08598c522fa6d
William-Dev <williamlegall5@gmail.com>
2025-01-17 11:46:20 +0100

    123

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Remplace 'Portfolio' par le nom de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
+  base: "/Portfolio/", // Indique à Vite le bon chemin pour GitHub Pages
 });
